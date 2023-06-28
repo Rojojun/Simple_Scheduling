53c2d3e93c9999b9e4271c9a951002cb9d87ff99
rojojun <zoloman316@gmail.com>
2023-06-28 23:07:15 +0900

    error, fail 작성 예정

--- src/main/java/com/example/scheduling/common/ApiCommonResponse.java
+++ src/main/java/com/example/scheduling/common/ApiCommonResponse.java
@@ -8,13 +8,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApiCommonResponse<T> {
 
-    private HealthType healthType;
+    private static HealthType healthType;
 
     private String status;
     private T data;
     private String message;
 
     public static <T> ApiCommonResponse<T> success(T data) {
+        return new ApiCommonResponse<>(healthType.SUCCESS.status, data, null);
+    }
 
+    public static ApiCommonResponse<?> noContent() {
+        return new ApiCommonResponse<>(healthType.SUCCESS.status, null, null)
+    }
+
+    private ApiCommonResponse(String status, T data, String message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
     }
 }
