c388f3043d34fa9afb2d31a1952fbda7d0e5cebb
rojojun <zoloman316@gmail.com>
2023-06-29 20:15:43 +0900

    fail 리스폰스 작성

--- src/main/java/com/example/scheduling/common/ApiCommonResponse.java
+++ src/main/java/com/example/scheduling/common/ApiCommonResponse.java
@@ -3,6 +3,15 @@ package com.example.scheduling.common;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,7 +28,21 @@ public class ApiCommonResponse<T> {
     }
 
     public static ApiCommonResponse<?> noContent() {
-        return new ApiCommonResponse<>(healthType.SUCCESS.status, null, null)
+        return new ApiCommonResponse<>(healthType.SUCCESS.status, null, null);
+    }
+
+    public static ApiCommonResponse<?> createFail(BindingResult bindingResult) {
+        Map<String, String> errors = new HashMap<>();
+
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
+        for (ObjectError error : allErrors) {
+            if (error instanceof FieldError) {
+                errors.put(((FieldError) error).getField(), error.getDefaultMessage());
+            } else {
+                errors.put(error.getObjectName(), error.getDefaultMessage());
+            }
+        }
+        return new ApiCommonResponse<>(healthType.FAIL.status, errors, null);
     }
 
     private ApiCommonResponse(String status, T data, String message) {
